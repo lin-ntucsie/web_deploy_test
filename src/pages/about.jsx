9430fdba50e2c4dad541e1e8fa7ed78c148c9864
Lin Howard <b10902004@ntu.edu.tw>
2023-04-01 13:13:56 +0900

    finish about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,3 +1,26 @@
 export default function About() {
-    return <h1>About</h1>;
+    return (
+        <section>
+            <div id = 'about'>
+                <ul id='first-order-list'>
+                    <li class='first-order'>Language Proficiency</li>
+                    <ul>
+                        <li>TOEIC(L&R) 880</li>
+                        <li>Japanese Language Proficiency Test (JLPT) N1</li>
+                        <li>Business Japanese Proficiency (BJT) J1</li>
+                    </ul>
+                    <li class='first-order'>Coding Skill</li>
+                    <ul>
+                        <li>Coding Language: C, C++, Python</li>
+                    </ul>
+                    <li class='first-order'>Contact</li>
+                    <ul>
+                        <li>+886916671216</li>
+                        <li>Taipei, Taiwan (R.O.C)</li>
+                        <li>b10902004@ntu.edu.tw</li>
+                    </ul>
+                </ul>
+            </div>
+        </section>
+    );
 }
